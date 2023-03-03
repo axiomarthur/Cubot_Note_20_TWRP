d3bffb5afdb20c0f1e1a86d6d8851343ec63f75a
Arthur <arthurvoigt800@@gmail.com>
2023-03-03 16:33:25 +0100

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+# Replace $$DEVICE$$ with your Device Name's Value.
+# Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
+# The part of last line in mine looks like "omni_Primo_RX5.mk"
+
+LOCAL_PATH := device/CUBOT/NOTE_20
+
+PRODUCT_MAKEFILES := $(LOCAL_PATH)/omni_NOTE_20.mk
---
+++ omni_NOTE_20.mk
@@ -0,0 +1,32 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Add this line if your device is 64-bit
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Another common config inclusion
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Replace $$DEVICE$$ with your Device Name's Value.
+# Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
+PRODUCT_COPY_FILES += device/CUBOT/NOTE_20/prebuilt/zImage:kernel
+# Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
+# PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
+
+PRODUCT_DEVICE := NOTE_20
+PRODUCT_NAME := omni_NOTE_20
+PRODUCT_BRAND := CUBOT
+PRODUCT_MODEL := CUBOT
+PRODUCT_MANUFACTURER := CUBOT
+
+# Forcefully add mtp support (adb is already there)
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# Add fingerprint from Stock ROM build.prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    # These lines are from my device. You MUST Replace yours.
+    BUILD_FINGERPRINT="CUBOT/NOTE_20_EEA/NOTE_20:10/QP1A.190711.020/1657817858:user/release-keys" \
+    PRIVATE_BUILD_DESC="NOTE_20_EEA-user 10 QP1A.190711.020 1657817858 release-keys"
